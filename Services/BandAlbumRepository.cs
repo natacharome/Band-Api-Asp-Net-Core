f838b9e1815d87115f412785a1a2ed1cdfe84ba3
Natacha Rome <n.rome@sumit.fr>
2020-04-28 15:04:57 +0200

    Setting up entities and dbContext

---
+++ Services/BandAlbumRepository.cs
@@ -0,0 +1,87 @@
+﻿using BandApi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BandApi.Services
+{
+    public class BandAlbumRepository : IBandAlbumRepository
+    {
+        private readonly BandAlbumContext _context;
+
+        public BandAlbumRepository(BandAlbumContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+        public void AddAlbum(Guid bandId, Album album)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddBand(Band band)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AlbumExists(Guid albumId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool BandExists(Guid bandId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteAlbum(Album album)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteBand(Band band)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Album GetAlbum(Guid bandId, Guid albumId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Album> GetAlbums(Guid bandId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Band GetBand(Guid bandId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Band> GetBands()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Band> GetBands(IEnumerable<Guid> bandIds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateAlbum(Album album)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateBand(Band band)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/IBandAlbumRepository.cs
@@ -0,0 +1,31 @@
+﻿using BandApi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BandApi.Services
+{
+    public interface IBandAlbumRepository
+    {
+        // Album
+        IEnumerable<Album> GetAlbums(Guid bandId);
+        Album GetAlbum(Guid bandId, Guid albumId);
+        void AddAlbum(Guid bandId, Album album);
+        void UpdateAlbum(Album album);
+        void DeleteAlbum(Album album);
+
+        // Band
+        IEnumerable<Band> GetBands();
+        Band GetBand(Guid bandId);
+        IEnumerable<Band> GetBands(IEnumerable<Guid> bandIds);
+        void AddBand(Band band);
+        void UpdateBand(Band band);
+        void DeleteBand(Band band);
+
+        // Both
+        bool BandExists(Guid bandId);
+        bool AlbumExists(Guid albumId);
+        bool Save();
+    }
+}
