de4e6a68ba18fa012561081f46e1c0110f4860df
Natacha Rome <n.rome@sumit.fr>
2020-04-28 15:41:26 +0200

    Database set up

--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using BandApi.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,7 +28,10 @@ namespace BandApi
         {
             services.AddControllers();
             services.AddScoped<IBandAlbumRepository, BandAlbumRepository>();
-            services.AddDbContext<BandAlbumContext>();
+            services.AddDbContext<BandAlbumContext>(options => 
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
