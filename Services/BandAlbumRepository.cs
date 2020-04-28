76da4358d9f7f7d2576a4944cb9a8d99fbdbe904
Natacha Rome <n.rome@sumit.fr>
2020-04-28 15:21:58 +0200

    Creating and implementing repository interface

--- Services/BandAlbumRepository.cs
+++ Services/BandAlbumRepository.cs
@@ -16,72 +16,107 @@ namespace BandApi.Services
         }
         public void AddAlbum(Guid bandId, Album album)
         {
-            throw new NotImplementedException();
+            if (bandId == Guid.Empty)
+                throw new ArgumentNullException(nameof(bandId));
+            if (album == null)
+                throw new ArgumentNullException(nameof(album));
+
+            album.BandId = bandId;
+            _context.Albums.Add(album);
         }
 
         public void AddBand(Band band)
         {
-            throw new NotImplementedException();
+            if (band == null)
+                throw new ArgumentNullException(nameof(band));
+            _context.Bands.Add(band);
         }
 
         public bool AlbumExists(Guid albumId)
         {
-            throw new NotImplementedException();
+            if(albumId == Guid.Empty)
+               throw new ArgumentNullException(nameof(albumId));
+            return _context.Albums.Any(a => a.Id == albumId);
         }
 
         public bool BandExists(Guid bandId)
         {
-            throw new NotImplementedException();
+            if (bandId == Guid.Empty)
+                throw new ArgumentNullException(nameof(bandId));
+
+            return _context.Bands.Any(b => b.Id == bandId);
         }
 
         public void DeleteAlbum(Album album)
         {
-            throw new NotImplementedException();
+            if (album == null)
+                throw new ArgumentNullException(nameof(album));
+           _context.Albums.Remove(album);
         }
 
         public void DeleteBand(Band band)
         {
-            throw new NotImplementedException();
+            if (band == null)
+                throw new ArgumentNullException(nameof(band));
+            _context.Bands.Remove(band);
         }
 
         public Album GetAlbum(Guid bandId, Guid albumId)
         {
-            throw new NotImplementedException();
+            if (bandId == Guid.Empty)
+                throw new ArgumentNullException(nameof(bandId));
+            if (albumId == Guid.Empty)
+                throw new ArgumentNullException(nameof(albumId));
+            return _context.Albums
+                .Where(a => a.BandId == bandId && a.Id == albumId)
+                .FirstOrDefault();
         }
 
         public IEnumerable<Album> GetAlbums(Guid bandId)
         {
-            throw new NotImplementedException();
+            if (bandId == Guid.Empty)
+                throw new ArgumentNullException(nameof(bandId));
+            return _context.Albums
+                .Where(a => a.BandId == bandId)
+                .OrderBy(a => a.Title)
+                .ToList();
         }
 
         public Band GetBand(Guid bandId)
         {
-            throw new NotImplementedException();
+            if (bandId == Guid.Empty)
+                throw new ArgumentNullException(nameof(bandId));
+            return _context.Bands.FirstOrDefault(b => b.Id == bandId);
         }
 
         public IEnumerable<Band> GetBands()
         {
-            throw new NotImplementedException();
+            return _context.Bands.ToList();
         }
 
         public IEnumerable<Band> GetBands(IEnumerable<Guid> bandIds)
         {
-            throw new NotImplementedException();
+            if (bandIds == null)
+                throw new ArgumentNullException(nameof(bandIds));
+            return _context.Bands
+                .Where(b => bandIds.Contains(b.Id))
+                .OrderBy(b => b.Name)
+                .ToList();
         }
 
         public bool Save()
         {
-            throw new NotImplementedException();
+            return (_context.SaveChanges() > 0);
         }
 
         public void UpdateAlbum(Album album)
         {
-            throw new NotImplementedException();
+            // Not implemented
         }
 
         public void UpdateBand(Band band)
         {
-            throw new NotImplementedException();
+            // Not implemented
         }
     }
 }
