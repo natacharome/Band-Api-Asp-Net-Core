0eb8f9d9995f9d944701478603d8bcce03ddfcc4
Natacha Rome <n.rome@sumit.fr>
2020-04-29 14:19:42 +0200

    Implements validationAttribute in a class

--- Dtos/AlbumForCreatingDto.cs
+++ Dtos/AlbumForCreatingDto.cs
@@ -1,13 +1,21 @@
-﻿using System;
+﻿using BandApi.ValidationAttributes;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BandApi.Dtos
 {
+    [TitleAndDescriptionAttribute]
     public class AlbumForCreatingDto
     {
+        [Required]
+        [MaxLength(200)]
         public string Title { get; set; }
+
+        [MaxLength(400)]
+
         public string Description { get; set; }
     }
 }
---
+++ ValidationAttributes/TitleAndDescriptionAttribute.cs
@@ -0,0 +1,24 @@
+﻿using BandApi.Dtos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BandApi.ValidationAttributes
+{
+    public class TitleAndDescriptionAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var album = (AlbumForCreatingDto)validationContext.ObjectInstance;
+            if(album.Title == album.Description)
+            {
+               return new ValidationResult("The Title and the description need to be different",
+                   new[] { "AlbumForCreatingDto" });
+            }
+            return ValidationResult.Success;
+       
+        }
+    }
+}
